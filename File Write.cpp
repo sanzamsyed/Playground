6bcb2ffcd400574f60f3ec7cb7ffce910bf7f068
sanzamsyed <46603047+sanzamsyed@users.noreply.github.com>
2019-07-04 02:47:50 +0600

    Add files via upload

---
+++ File Write.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ifstream file;
+    ofstream newFile;
+    string fileName = "";
+    string newfileName = "";
+
+
+    fileName = "file.txt";
+    newfileName = "newFile.txt";
+
+    file.open(fileName);
+    newFile.open(newfileName);
+
+
+    /*
+        Reading from the file line by line
+    */
+
+    string line;
+    int cnt = 1;
+
+    while(getline(file,line))
+    {
+
+        newFile << cnt++ << "\t" << line.c_str() << endl;
+    }
+
+    file.close();
+    newFile.close();
+
+
+}
