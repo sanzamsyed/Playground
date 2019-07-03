922dd5f40ac036d8303fffc623b4ac6e3f6723c3
Sanzam Syed <sanzamsyed71@gmail.com>
2019-07-04 02:19:45 +0600

    First Commit

---
+++ File.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ifstream file;
+    string fileName = "";
+
+
+    fileName = "file.txt";
+
+    file.open(fileName);
+
+
+    /*
+        Reading from the file line by line
+    */
+
+    string line;
+
+    while(getline(file,line))
+    {
+        printf("%s",line.c_str());
+        cout << endl;
+    }
+
+    file.close();
+
+
+    /*
+        Reading from the file word by word
+    */
+
+
+    file.open(fileName.c_str());
+    string word;
+
+    while(file >> word)
+    {
+        cout << word << endl;
+    }
+
+    file.close();
+
+
+
+
+
+
+}
